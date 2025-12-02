994a4af0d94a92dd18faa1c116ce68088ddd2e3e
nam-joshi <namrata.joshi1998@gmail.com>
2025-12-01 22:40:06 -0800

    Add search bar visibility test to CSUF homepage suite

--- cypress/e2e/CSUF-homepage.cy.js
+++ cypress/e2e/CSUF-homepage.cy.js
@@ -3,4 +3,8 @@ describe('CSUF Homepage Smoke Test', () => {
     cy.visit('https://www.fullerton.edu/');
     cy.title().should('include', 'Fullerton');
   });
+  it("checks that the search bar is visible on homepage", () => {
+    cy.visit("https://www.fullerton.edu");
+    cy.get("input[type='search'], #search, .search").should("exist");
+  });
 });
